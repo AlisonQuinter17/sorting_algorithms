77c1bd11fda8c04947e88579bee6ce797c51c2c1
Alison Quintero <alisondanellaquintero@hotmail.com>
2020-10-20 22:46:43 -0700

    Header file.

--- sort.h
+++ sort.h
@@ -14,9 +14,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* Prototypes (Basic) */
@@ -26,6 +26,9 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void quicksort_recursion(int *array, size_t size, ssize_t first, ssize_t last);
+size_t partition(int *array, size_t size, ssize_t first, ssize_t last);
+void swap(int *array, size_t size, int *value1, int *value2);
 
 /* Prototypes (Advanced) */
 void shell_sort(int *array, size_t size);
